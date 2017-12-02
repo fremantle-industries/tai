22e0983fcd95430a87ca31278782da31f41f3ff4
Alex Kwiatkowski <alex+git@rival-studios.com>
2017-12-02 10:57:35 -0800

    Switch back to ex_gdax mainline package

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule Tai.Mixfile do
   defp deps do
     [
       {:decimal, "~> 1.0"},
-      {:ex_gdax, github: "fremantle-capital/ex_gdax", branch: "improvements"},
+      {:ex_gdax, "~> 0.1.2"},
       {:httpoison, "~> 0.12"},
       {:json, "~> 1.0"},
       {:timex, "~> 3.1"},
