8ea64dfcc7fc656bd4ce43b6c53eb4ac43c19bc4
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-01-21 21:26:33 -0800

    Upgrade Elixir to depend on 1.6

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Tai.Mixfile do
     [
       app: :tai,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
