1e4a2306e4633d13e1d4814611f2d4920072865e
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-06-19 20:21:36 -0700

    Add tighter spec definition for WebSocket

--- lib/tai/web_socket.ex
+++ lib/tai/web_socket.ex
@@ -1,10 +1,23 @@
 defmodule Tai.WebSocket do
+  @spec send_msg(pid, binary) ::
+          :ok
+          | {:error,
+             %WebSockex.FrameEncodeError{}
+             | %WebSockex.ConnError{}
+             | %WebSockex.NotConnectedError{}
+             | %WebSockex.InvalidFrameError{}}
   def send_msg(pid, msg) do
     WebSockex.send_frame(pid, {:text, msg})
   end
 
-  @spec send_json_msg(pid, map) :: :ok | {:error, atom}
-  def send_json_msg(pid, msg) do
+  @spec send_json_msg(pid, map) ::
+          :ok
+          | {:error,
+             %WebSockex.FrameEncodeError{}
+             | %WebSockex.ConnError{}
+             | %WebSockex.NotConnectedError{}
+             | %WebSockex.InvalidFrameError{}}
+  def(send_json_msg(pid, msg)) do
     send_msg(pid, msg |> Poison.encode!())
   end
 end
