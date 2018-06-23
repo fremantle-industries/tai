1df8c42d72afa491b8033bd98d421197b5d0845c
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-06-22 21:08:33 -0700

    Use module namespace instead of alias in advisors config test

--- test/tai/advisors/config_test.exs
+++ test/tai/advisors/config_test.exs
@@ -2,10 +2,8 @@ defmodule Tai.Advisors.ConfigTest do
   use ExUnit.Case
   doctest Tai.Advisors.Config
 
-  alias Tai.Advisors.Config
-
   test "all returns the application config" do
-    assert Config.all() == [
+    assert Tai.Advisors.Config.all() == [
              %{
                id: :create_and_cancel_pending_order,
                supervisor: Examples.Advisors.CreateAndCancelPendingOrder.Supervisor,
@@ -25,22 +23,22 @@ defmodule Tai.Advisors.ConfigTest do
   end
 
   test "find returns the config for the matching advisor or nil if it doesn't exist" do
-    assert Config.find(:log_spread_advisor) == %{
+    assert Tai.Advisors.Config.find(:log_spread_advisor) == %{
              id: :log_spread_advisor,
              supervisor: Examples.Advisors.LogSpread.Supervisor,
              order_books: "*"
            }
 
-    assert Config.find(:i_dont_exist) == nil
+    assert Tai.Advisors.Config.find(:i_dont_exist) == nil
   end
 
   test "order_books returns a map of feeds with books for the given advisor id" do
-    assert Config.order_books(:log_spread_advisor) == %{
+    assert Tai.Advisors.Config.order_books(:log_spread_advisor) == %{
              test_feed_a: [:btc_usd, :ltc_usd],
              test_feed_b: [:eth_usd, :ltc_usd]
            }
 
-    assert Config.order_books(:create_and_cancel_pending_order) == %{
+    assert Tai.Advisors.Config.order_books(:create_and_cancel_pending_order) == %{
              test_feed_a: [:btc_usd, :ltc_usd],
              test_feed_b: [:eth_usd]
            }
