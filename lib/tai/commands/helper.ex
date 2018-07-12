2382afb6d673a111e779c4975e1cf11c38a5c897
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-07-11 19:07:04 -0700

    Use module namespace instead of alias for commands helper

--- lib/tai/commands/helper.ex
+++ lib/tai/commands/helper.ex
@@ -3,29 +3,27 @@ defmodule Tai.Commands.Helper do
   Commands for using `tai` in IEx
   """
 
-  alias Tai.Commands
-
   @spec help :: no_return
-  defdelegate help, to: Commands.Info
+  defdelegate help, to: Tai.Commands.Info
 
   @spec balance :: no_return
-  defdelegate balance, to: Commands.Balance
+  defdelegate balance, to: Tai.Commands.Balance
 
   @spec markets :: no_return
-  defdelegate markets, to: Commands.Markets
+  defdelegate markets, to: Tai.Commands.Markets
 
   @spec orders :: no_return
-  defdelegate orders, to: Commands.Orders
+  defdelegate orders, to: Tai.Commands.Orders
 
   @spec buy_limit(atom, atom, float, float, atom) :: no_return
-  defdelegate buy_limit(account_id, symbol, price, size, time_in_force), to: Commands.Trading
+  defdelegate buy_limit(account_id, symbol, price, size, time_in_force), to: Tai.Commands.Trading
 
   @spec sell_limit(atom, atom, float, float, atom) :: no_return
-  defdelegate sell_limit(account_id, symbol, price, size, time_in_force), to: Commands.Trading
+  defdelegate sell_limit(account_id, symbol, price, size, time_in_force), to: Tai.Commands.Trading
 
   @spec order_status(atom, String.t()) :: no_return
-  defdelegate order_status(account_id, order_id), to: Commands.Trading
+  defdelegate order_status(account_id, order_id), to: Tai.Commands.Trading
 
   @spec cancel_order(atom, String.t()) :: no_return
-  defdelegate cancel_order(account_id, order_id), to: Commands.Trading
+  defdelegate cancel_order(account_id, order_id), to: Tai.Commands.Trading
 end
