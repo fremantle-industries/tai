52b07094c78c0af0339235d8f0ab36bfcdb6d33f
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-07-21 14:11:15 -0700

    Simplify exchange adapters supervisor

--- lib/tai/exchanges/accounts_supervisor.ex
+++ lib/tai/exchanges/accounts_supervisor.ex
@@ -5,22 +5,20 @@ defmodule Tai.Exchanges.AccountsSupervisor do
 
   use Supervisor
 
-  alias Tai.Exchanges
-
   def start_link(_) do
     Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
-    Exchanges.Config.account_ids()
+    Tai.Exchanges.Config.account_ids()
     |> Enum.map(&account_supervisor_spec/1)
     |> Supervisor.init(strategy: :one_for_one)
   end
 
   defp account_supervisor_spec(account_id) do
     Supervisor.child_spec(
-      {Exchanges.AccountSupervisor, account_id},
-      id: "#{Exchanges.AccountSupervisor}_#{account_id}"
+      {Tai.Exchanges.AccountSupervisor, account_id},
+      id: "#{Tai.Exchanges.AccountSupervisor}_#{account_id}"
     )
   end
 end
--- lib/tai/exchanges/adapters_supervisor.ex
+++ lib/tai/exchanges/adapters_supervisor.ex
@@ -2,19 +2,12 @@ defmodule Tai.Exchanges.AdaptersSupervisor do
   use Supervisor
 
   def start_link(_) do
-    Supervisor.start_link(
-      __MODULE__,
-      :ok,
-      name: __MODULE__
-    )
+    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def init(:ok) do
     Tai.Exchanges.Config.all()
-    |> Enum.map(fn %Tai.Exchanges.Config{} = config ->
-      {config.supervisor, config}
-      |> Supervisor.child_spec(id: config.id)
-    end)
+    |> Enum.map(&Supervisor.child_spec({&1.supervisor, &1}, id: &1.id))
     |> Supervisor.init(strategy: :one_for_one)
   end
 end
