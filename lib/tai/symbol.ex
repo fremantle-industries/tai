380933ef645bd89e75e63e9b9c9beae12de12fbf
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-08-05 17:01:38 -0700

    Add Symbol.base_and_quote

--- lib/tai/symbol.ex
+++ lib/tai/symbol.ex
@@ -37,4 +37,24 @@ defmodule Tai.Symbol do
     |> String.downcase()
     |> String.to_atom()
   end
+
+  def base_and_quote(symbol) when is_binary(symbol) do
+    assets = String.split(symbol, "_")
+    assets_count = Enum.count(assets)
+
+    if assets_count == 2 do
+      [base_asset, quote_asset] = Enum.map(assets, &String.to_atom/1)
+      {:ok, {base_asset, quote_asset}}
+    else
+      {:error, :symbol_format_must_be_base_quote}
+    end
+  end
+
+  def base_and_quote(symbol) when is_atom(symbol) do
+    symbol
+    |> Atom.to_string()
+    |> base_and_quote
+  end
+
+  def base_and_quote(_), do: {:error, :symbol_must_be_an_atom_or_string}
 end
--- test/tai/symbol_test.exs
+++ test/tai/symbol_test.exs
@@ -2,7 +2,7 @@ defmodule Tai.SymbolTest do
   use ExUnit.Case, async: true
   doctest Tai.Symbol
 
-  describe "#downcase" do
+  describe ".downcase" do
     test "converts an atom to a downcase string" do
       assert Tai.Symbol.downcase(:FOO) == "foo"
     end
@@ -12,7 +12,7 @@ defmodule Tai.SymbolTest do
     end
   end
 
-  describe "#downcase_all" do
+  describe ".downcase_all" do
     test "converts atoms to downcase strings" do
       assert Tai.Symbol.downcase_all([:FOO, :Bar]) == ["foo", "bar"]
     end
@@ -22,7 +22,7 @@ defmodule Tai.SymbolTest do
     end
   end
 
-  describe "#upcase" do
+  describe ".upcase" do
     test "converts an atom to an uppercase string" do
       assert Tai.Symbol.upcase(:foo) == "FOO"
     end
@@ -32,13 +32,29 @@ defmodule Tai.SymbolTest do
     end
   end
 
-  describe "#build" do
+  describe ".build" do
     test "returns a symbol for the base and quote asset separated by an underscore" do
       assert Tai.Symbol.build("btc", "usdt") == :btc_usdt
     end
 
     test "downcases the base and quote assets" do
       assert Tai.Symbol.build("BTC", "USDT") == :btc_usdt
+    end
+  end
+
+  describe ".base_and_quote" do
+    test "returns the base and quote asset in an ok tuple" do
+      assert Tai.Symbol.base_and_quote(:btc_usdt) == {:ok, {:btc, :usdt}}
+      assert Tai.Symbol.base_and_quote("btc_usdt") == {:ok, {:btc, :usdt}}
+    end
+
+    test "returns an error tuple when the symbol is not an atom" do
+      assert Tai.Symbol.base_and_quote(10) == {:error, :symbol_must_be_an_atom_or_string}
+    end
+
+    test "returns an error tuple when the symbol isn't 2 assets separated by an _" do
+      assert Tai.Symbol.base_and_quote(:btc_ltc_eth) ==
+               {:error, :symbol_format_must_be_base_quote}
     end
   end
 end
