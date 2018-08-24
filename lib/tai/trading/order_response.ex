b26db7adf41f5c82e376fec8cc3ec3e1e19f35bb
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-08-24 16:14:08 -0700

    Add attrs to type definition of OrderResponse

--- lib/tai/trading/order_response.ex
+++ lib/tai/trading/order_response.ex
@@ -6,7 +6,13 @@ defmodule Tai.Trading.OrderResponse do
   @typedoc """
   Details of an order executed on an account 
   """
-  @type t :: Tai.Trading.OrderResponse
+  @type t :: %Tai.Trading.OrderResponse{
+          id: String.t(),
+          status: atom,
+          original_size: Decimal.t(),
+          time_in_force: atom,
+          executed_size: Decimal.t() | nil
+        }
 
   @enforce_keys [:id, :status, :original_size, :time_in_force, :executed_size]
   defstruct [:id, :status, :original_size, :time_in_force, :executed_size]
