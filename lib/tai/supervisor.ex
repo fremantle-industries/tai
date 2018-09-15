75a7496838865310e45b1e180a8c99e8dfb00a9c
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-09-15 14:30:26 -0700

    Start exchange adapter processes in root supervisor

--- lib/tai/supervisor.ex
+++ lib/tai/supervisor.ex
@@ -13,9 +13,15 @@ defmodule Tai.Supervisor do
     children = [
       Tai.PubSub,
       {Tai.Settings, Tai.Config.all()},
+      Tai.Exchanges.Products,
+      Tai.Exchanges.Fees,
       Tai.Trading.OrderStore,
       Tai.Advisors.Supervisor,
-      Tai.Exchanges.Supervisor
+      Tai.Exchanges.AdaptersSupervisor,
+      # TODO
+      # OrderBookFeedsSupervisor will become the responsibility of each 
+      # individual adapter supervisor. Once complete it can be removed.
+      Tai.Exchanges.OrderBookFeedsSupervisor
     ]
 
     Supervisor.init(children, strategy: :one_for_one)
