ff0e0512dc9d726c24720795faa49bf5254b97f6
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-09-28 14:10:51 -0700

    Move root supervisor into application

--- lib/tai.ex
+++ lib/tai.ex
@@ -2,6 +2,22 @@ defmodule Tai do
   use Application
 
   def start(_type, _args) do
-    Tai.Supervisor.start_link()
+    children = [
+      Tai.PubSub,
+      {Tai.Settings, Tai.Config.all()},
+      Tai.Exchanges.Products,
+      Tai.Exchanges.Fees,
+      Tai.Exchanges.AssetBalances,
+      Tai.Trading.OrderStore,
+      Tai.Advisors.Supervisor,
+      Tai.Exchanges.AdaptersSupervisor,
+      # TODO
+      # OrderBookFeedsSupervisor will become the responsibility of each 
+      # individual adapter supervisor. Once complete it can be removed.
+      Tai.Exchanges.OrderBookFeedsSupervisor
+    ]
+
+    opts = [strategy: :one_for_one, name: Tai.Supervisor]
+    Supervisor.start_link(children, opts)
   end
 end
