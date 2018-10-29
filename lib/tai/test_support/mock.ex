fd1ef01b1bb801df9cab05c9d57104ae3563ac1e
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-10-28 22:22:16 -0700

    Improve specs for test support mock

--- lib/tai/test_support/mock.ex
+++ lib/tai/test_support/mock.ex
@@ -1,25 +1,27 @@
 defmodule Tai.TestSupport.Mock do
   @type location :: Tai.Markets.Location.t()
+  @type product :: Tai.Exchanges.Product.t()
+  @type fee_info :: Tai.Exchanges.FeeInfo.t()
 
-  @spec mock_product(Tai.Exchanges.Product.t() | map) :: :ok
+  @spec mock_product(product | map) :: :ok
   def mock_product(%Tai.Exchanges.Product{} = product) do
     product
     |> Tai.Exchanges.ProductStore.upsert()
   end
 
-  def mock_product(%{} = attrs) do
+  def mock_product(attrs) when is_map(attrs) do
     Tai.Exchanges.Product
     |> struct(attrs)
     |> Tai.Exchanges.ProductStore.upsert()
   end
 
-  @spec mock_fee_info(Tai.Exchanges.FeeInfo.t() | map) :: :ok
+  @spec mock_fee_info(fee_info | map) :: :ok
   def mock_fee_info(%Tai.Exchanges.FeeInfo{} = fee_info) do
     fee_info
     |> Tai.Exchanges.FeeStore.upsert()
   end
 
-  def mock_fee_info(%{} = attrs) do
+  def mock_fee_info(attrs) when is_map(attrs) do
     Tai.Exchanges.FeeInfo
     |> struct(attrs)
     |> Tai.Exchanges.FeeStore.upsert()
