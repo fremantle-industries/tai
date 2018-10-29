d868815a675276bb65e8c9482eda68b9d8bb2cac
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-10-28 22:35:06 -0700

    Support size and server_changed_at in mock order book feed adapter

--- lib/tai/venue_adapters/mock/order_book_feed/snapshot.ex
+++ lib/tai/venue_adapters/mock/order_book_feed/snapshot.ex
@@ -3,11 +3,22 @@ defmodule Tai.VenueAdapters.Mock.OrderBookFeed.Snapshot do
     snapshot_side
     |> Enum.reduce(
       %{},
-      fn {price_str, size}, acc ->
-        with {price, ""} <- Float.parse(price_str) do
-          Map.put(acc, price, {size, processed_at, nil})
-        end
-      end
+      &add_change_point(&1, processed_at, &2)
     )
+  end
+
+  defp add_change_point(
+         {price_str, %{"size" => size, "server_changed_at" => changed_str}},
+         processed_at,
+         acc
+       ) do
+    {price, ""} = Float.parse(price_str)
+    {:ok, server_changed_at, 0} = DateTime.from_iso8601(changed_str)
+    Map.put(acc, price, {size, processed_at, server_changed_at})
+  end
+
+  defp add_change_point({price_str, size}, processed_at, acc) do
+    {price, ""} = Float.parse(price_str)
+    Map.put(acc, price, {size, processed_at, nil})
   end
 end
