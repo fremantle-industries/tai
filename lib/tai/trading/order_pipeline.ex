a30a3adafa7910f7e5055589e04cd50ab0960dc7
Alex Kwiatkowski <alex+git@rival-studios.com>
2018-12-06 19:51:07 -0800

    Delegate OrderPipeline enqueue

--- lib/tai/trading/order_pipeline.ex
+++ lib/tai/trading/order_pipeline.ex
@@ -1,17 +1,6 @@
 defmodule Tai.Trading.OrderPipeline do
   alias Tai.Trading
 
-  @type order :: Trading.Order.t()
-  @type submission :: Tai.Trading.OrderStore.submission()
-
-  @spec enqueue(submission) :: order
-  def enqueue(submission) do
-    submission
-    |> Trading.OrderPipeline.Enqueue.execute_step()
-  end
-
-  @doc """
-  Cancel a pending order
-  """
+  defdelegate enqueue(submission), to: Trading.OrderPipeline.Enqueue, as: :execute_step
   defdelegate cancel(order), to: Trading.OrderPipeline.Cancel, as: :execute_step
 end
