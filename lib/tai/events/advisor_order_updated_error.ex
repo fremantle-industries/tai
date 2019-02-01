c6c4d579ccb7b55f36d1346feac9b970a3006733
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-02-01 13:31:43 -0800

    Transform error & stacktrace to string when logging

--- lib/tai/events/advisor_order_updated_error.ex
+++ lib/tai/events/advisor_order_updated_error.ex
@@ -2,3 +2,17 @@ defmodule Tai.Events.AdvisorOrderUpdatedError do
   @enforce_keys [:error, :stacktrace]
   defstruct [:error, :stacktrace]
 end
+
+defimpl Tai.LogEvent, for: Tai.Events.AdvisorOrderUpdatedError do
+  def to_data(event) do
+    keys =
+      event
+      |> Map.keys()
+      |> Enum.filter(&(&1 != :__struct__))
+
+    event
+    |> Map.take(keys)
+    |> Map.put(:error, event.error |> inspect)
+    |> Map.put(:stacktrace, event.stacktrace |> inspect)
+  end
+end
