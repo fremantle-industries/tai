c89bcd2ec091ee0e7c74526e39199c3cae25afc9
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-04-06 11:12:49 -0700

    Bump dialyxir & ex_doc dependencies

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,4 +1,25 @@
 [
-  {":0:unknown_function Function dont_display_result/0 does not exist.", :unknown_function},
-  {":0:unknown_function Function __impl__/1 does not exist.", :unknown_function}
+  {":0:unknown_function Function IEx.dont_display_result/0 does not exist.", :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Atom\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.BitString\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Float\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Function\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Integer\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.List\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Map\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.PID\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Port\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Reference\.__impl__/1 does not exist.",
+   :unknown_function},
+  {":0:unknown_function Function Tai\.LogEvent\.Tuple\.__impl__/1 does not exist.",
+   :unknown_function}
 ]
