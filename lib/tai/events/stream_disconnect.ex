13e663d7536c1fa29e34dae640b4afba26eb9810
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-04-11 21:14:04 -0700

    Inspect reason for stream disconnect event

--- lib/tai/events/stream_disconnect.ex
+++ lib/tai/events/stream_disconnect.ex
@@ -5,3 +5,16 @@ defmodule Tai.Events.StreamDisconnect do
   @enforce_keys ~w(venue reason)a
   defstruct ~w(venue reason)a
 end
+
+defimpl Tai.LogEvent, for: Tai.Events.StreamDisconnect do
+  def to_data(event) do
+    keys =
+      event
+      |> Map.keys()
+      |> Enum.filter(&(&1 != :__struct__))
+
+    event
+    |> Map.take(keys)
+    |> Map.put(:reason, event.reason |> inspect)
+  end
+end
---
+++ test/tai/events/stream_disconnect_test.exs
@@ -0,0 +1,16 @@
+defmodule Tai.Events.StreamDisconnectTest do
+  use ExUnit.Case, async: true
+
+  @base_attrs %{
+    venue: :venue_a,
+    reason: {:remote, :normal}
+  }
+
+  test ".to_data/1 transforms reason to a string" do
+    event = struct!(Tai.Events.StreamDisconnect, @base_attrs)
+
+    assert %{} = json = Tai.LogEvent.to_data(event)
+    assert json.venue == :venue_a
+    assert json.reason == "{:remote, :normal}"
+  end
+end
