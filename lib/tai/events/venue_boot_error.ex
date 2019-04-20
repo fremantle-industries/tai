c8afdf70b0151ee13f0a4f1e103317f3a0895b44
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-04-19 19:22:50 -0700

    Transform venue boot error reason to string

--- lib/tai/events/venue_boot_error.ex
+++ lib/tai/events/venue_boot_error.ex
@@ -7,3 +7,16 @@ defmodule Tai.Events.VenueBootError do
   @enforce_keys ~w(venue reason)a
   defstruct ~w(venue reason)a
 end
+
+defimpl Tai.LogEvent, for: Tai.Events.VenueBootError do
+  def to_data(event) do
+    keys =
+      event
+      |> Map.keys()
+      |> Enum.filter(&(&1 != :__struct__))
+
+    event
+    |> Map.take(keys)
+    |> Map.put(:reason, event.reason |> inspect)
+  end
+end
