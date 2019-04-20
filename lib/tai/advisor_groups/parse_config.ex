c90548d4181d070089fbe796a77b32d4f0a14d97
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-04-19 19:45:50 -0700

    Extract build_group function when parsing advisor groups

--- lib/tai/advisor_groups/parse_config.ex
+++ lib/tai/advisor_groups/parse_config.ex
@@ -5,34 +5,30 @@ defmodule Tai.AdvisorGroups.ParseConfig do
 
   @spec parse_config(config) :: {:ok, [advisor_group]} | {:error, map}
   def parse_config(%Tai.Config{advisor_groups: advisor_groups}) do
-    {groups, errors} =
-      advisor_groups
-      |> Enum.reduce(
-        {[], %{}},
-        fn {id, config}, {groups, errors} ->
-          group = %Tai.AdvisorGroup{
-            id: id,
-            advisor: config |> Keyword.get(:advisor),
-            factory: config |> Keyword.get(:factory),
-            products: config |> Keyword.get(:products),
-            config: config |> Keyword.get(:config, %{})
-          }
-
-          if Vex.valid?(group) do
-            new_groups = groups ++ [group]
-            {new_groups, errors}
-          else
-            group_errors =
-              group
-              |> Vex.errors()
-              |> Enum.map(fn {:error, k, _, m} -> {k, m} end)
-
-            new_errors = Map.put(errors, id, group_errors)
-            {groups, new_errors}
-          end
-        end
-      )
-
+    {groups, errors} = advisor_groups |> Enum.reduce({[], %{}}, &build_group/2)
     if Enum.empty?(errors), do: {:ok, groups}, else: {:error, errors}
+  end
+
+  defp build_group({id, config}, {groups, errors}) do
+    group = %Tai.AdvisorGroup{
+      id: id,
+      advisor: config |> Keyword.get(:advisor),
+      factory: config |> Keyword.get(:factory),
+      products: config |> Keyword.get(:products),
+      config: config |> Keyword.get(:config, %{})
+    }
+
+    if Vex.valid?(group) do
+      new_groups = groups ++ [group]
+      {new_groups, errors}
+    else
+      group_errors =
+        group
+        |> Vex.errors()
+        |> Enum.map(fn {:error, k, _, m} -> {k, m} end)
+
+      new_errors = Map.put(errors, id, group_errors)
+      {groups, new_errors}
+    end
   end
 end
