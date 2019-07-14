3b14489d49843f811c3fbdd8f15dad5cf6051bda
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-07-14 12:34:51 -0700

    Release 0.0.24

--- README.md
+++ README.md
@@ -75,7 +75,7 @@ config :tai,
 ```elixir
 def deps do
   [
-    {:tai, "~> 0.0.23"}
+    {:tai, "~> 0.0.24"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tai.Mixfile do
   def project do
     [
       app: :tai,
-      version: "0.0.23",
+      version: "0.0.24",
       elixir: "~> 1.8",
       package: package(),
       start_permanent: Mix.env() == :prod,
