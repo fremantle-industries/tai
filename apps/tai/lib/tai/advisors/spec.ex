68504c542e63984c0d38bee00ab8c2a2e364d27e
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-07-28 15:32:58 -0700

    Advisor spec is restart :temporary

--- apps/tai/lib/tai/advisors/spec.ex
+++ apps/tai/lib/tai/advisors/spec.ex
@@ -28,12 +28,13 @@ defmodule Tai.Advisors.Spec do
   @enforce_keys ~w(mod start_on_boot group_id advisor_id products config)a
   defstruct ~w(mod start_on_boot group_id advisor_id products config run_store trades)a
 
-  @spec to_child_spec(t) :: {mod, spec_opts}
+  @spec to_child_spec(t) :: Supervisor.child_spec()
   def to_child_spec(spec) do
     run_store = spec.run_store || %{}
     trades = spec.trades || []
+    name = Tai.Advisor.to_name(spec.group_id, spec.advisor_id)
 
-    opts = [
+    start_opts = [
       group_id: spec.group_id,
       advisor_id: spec.advisor_id,
       products: spec.products,
@@ -42,6 +43,12 @@ defmodule Tai.Advisors.Spec do
       trades: trades
     ]
 
-    {spec.mod, opts}
+    %{
+      id: name,
+      start: {spec.mod, :start_link, [start_opts]},
+      type: :worker,
+      restart: :temporary,
+      shutdown: 5000
+    }
   end
 end
