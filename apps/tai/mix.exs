8f761c926c96608fb72eba9406cf7c97d6492a2c
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-08-11 08:52:22 -0400

    Release 0.0.29

--- apps/tai/mix.exs
+++ apps/tai/mix.exs
@@ -4,7 +4,7 @@ defmodule Tai.Mixfile do
   def project do
     [
       app: :tai,
-      version: "0.0.28",
+      version: "0.0.29",
       elixir: "~> 1.8",
       package: package(),
       start_permanent: Mix.env() == :prod,
