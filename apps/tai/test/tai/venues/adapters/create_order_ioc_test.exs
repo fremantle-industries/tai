f7a86084f64956ca39c896910703f66d3c71d97c
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-08-23 20:43:55 -0700

    Add IoC orders to Binance venue adapter

--- apps/tai/test/tai/venues/adapters/create_order_ioc_test.exs
+++ apps/tai/test/tai/venues/adapters/create_order_ioc_test.exs
@@ -83,7 +83,7 @@ defmodule Tai.Venues.Adapters.CreateOrderIocTest do
       client_id: Ecto.UUID.generate(),
       venue_id: venue_id,
       account_id: :main,
-      symbol: venue_id |> product_symbol,
+      product_symbol: venue_id |> product_symbol,
       side: side,
       price: venue_id |> price(side, time_in_force, action),
       qty: venue_id |> qty(side, time_in_force, action),
--- config/test.exs
+++ config/test.exs
@@ -106,7 +106,7 @@ config(:tai, :test_venue_adapters_maker_taker_fees, [:mock, :binance, :poloniex,
 config(:tai, :test_venue_adapters_create_order_gtc_open, [:bitmex, :binance])
 config(:tai, :test_venue_adapters_create_order_gtc_accepted, [:okex_futures, :okex_swap])
 config(:tai, :test_venue_adapters_create_order_fok, [:bitmex, :binance])
-config(:tai, :test_venue_adapters_create_order_ioc, [:bitmex])
+config(:tai, :test_venue_adapters_create_order_ioc, [:bitmex, :binance])
 config(:tai, :test_venue_adapters_create_order_close, [:okex_futures, :okex_swap])
 
 config(:tai, :test_venue_adapters_create_order_error_insufficient_balance, [
