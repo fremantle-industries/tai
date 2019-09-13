0634c645630e8438b9852456ed6dcf5f837338a2
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-13 09:54:53 -0700

    Release v0.0.36

--- apps/examples/mix.exs
+++ apps/examples/mix.exs
@@ -4,7 +4,7 @@ defmodule Examples.MixProject do
   def project do
     [
       app: :examples,
-      version: "0.0.35",
+      version: "0.0.36",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- apps/tai/mix.exs
+++ apps/tai/mix.exs
@@ -4,7 +4,7 @@ defmodule Tai.Mixfile do
   def project do
     [
       app: :tai,
-      version: "0.0.35",
+      version: "0.0.36",
       elixir: "~> 1.8",
       package: package(),
       start_permanent: Mix.env() == :prod,
