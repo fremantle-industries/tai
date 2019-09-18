a07217c2f1c13960d53fa9a7aae10e09e6ac2036
Yuri Kovalov <yuri@yurikoval.com>
2019-09-18 13:40:12 +0800

    Test swap, futures, spot trade feeds individually

--- apps/tai/test/tai/venue_adapters/okex/stream/trades_test.exs
+++ apps/tai/test/tai/venue_adapters/okex/stream/trades_test.exs
@@ -10,30 +10,69 @@ defmodule Tai.VenueAdapters.OkEx.Stream.TradeTest do
     :ok
   end
 
-  ~w(futures spot swap)
-  |> Enum.map(&"#{&1}/trade")
-  |> Enum.map(fn table ->
-    @table table
-    test "broadcasts an event when public trade is received from #{@table}" do
-      Events.firehose_subscribe()
-      venue_trade_id = Ecto.UUID.generate()
+  test "broadcasts an event when public trade is received from futures/trade" do
+    Events.firehose_subscribe()
+    venue_trade_id = Ecto.UUID.generate()
 
-      venue_trades = [
-        %{
-          "side" => "buy",
-          "trade_id" => venue_trade_id,
-          "price" => "5556.91",
-          "qty" => "5",
-          "instrument_id" => "BTC-USD-190628",
-          "timestamp" => "2019-05-06T07:19:37.496Z"
-        }
-      ]
+    venue_trades = [
+      %{
+        "side" => "buy",
+        "trade_id" => venue_trade_id,
+        "price" => "5556.91",
+        "qty" => "5",
+        "instrument_id" => "BTC-USD-190628",
+        "timestamp" => "2019-05-06T07:19:37.496Z"
+      }
+    ]
 
-      :my_venue
-      |> ProcessOptionalChannels.to_name()
-      |> GenServer.cast({%{"table" => @table, "data" => venue_trades}, :ignore})
+    :my_venue
+    |> ProcessOptionalChannels.to_name()
+    |> GenServer.cast({%{"table" => "futures/trade", "data" => venue_trades}, :ignore})
 
-      assert_event(%Events.Trade{venue_trade_id: venue_trade_id})
-    end
-  end)
+    assert_event(%Events.Trade{venue_trade_id: venue_trade_id})
+  end
+
+  test "broadcasts an event when public trade is received from swap/trade" do
+    Events.firehose_subscribe()
+    venue_trade_id = Ecto.UUID.generate()
+
+    venue_trades = [
+      %{
+        "side" => "buy",
+        "trade_id" => venue_trade_id,
+        "price" => "5556.91",
+        "size" => "5",
+        "instrument_id" => "BTC-USD-SWAP",
+        "timestamp" => "2019-05-06T07:19:37.496Z"
+      }
+    ]
+
+    :my_venue
+    |> ProcessOptionalChannels.to_name()
+    |> GenServer.cast({%{"table" => "swap/trade", "data" => venue_trades}, :ignore})
+
+    assert_event(%Events.Trade{venue_trade_id: venue_trade_id})
+  end
+
+  test "broadcasts an event when public trade is received from spot/trade" do
+    Events.firehose_subscribe()
+    venue_trade_id = Ecto.UUID.generate()
+
+    venue_trades = [
+      %{
+        "side" => "buy",
+        "trade_id" => venue_trade_id,
+        "price" => "5556.91",
+        "size" => "5",
+        "instrument_id" => "TRX-BTC",
+        "timestamp" => "2019-05-06T07:19:37.496Z"
+      }
+    ]
+
+    :my_venue
+    |> ProcessOptionalChannels.to_name()
+    |> GenServer.cast({%{"table" => "spot/trade", "data" => venue_trades}, :ignore})
+
+    assert_event(%Events.Trade{venue_trade_id: venue_trade_id})
+  end
 end
