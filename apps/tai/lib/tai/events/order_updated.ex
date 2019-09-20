c00812afd5b9bee766116a11212ae8b23b098b0b
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-20 11:48:31 -0700

    Don't serialize order update decimals when nil

--- apps/tai/lib/tai/events/order_updated.ex
+++ apps/tai/lib/tai/events/order_updated.ex
@@ -77,10 +77,13 @@ defimpl Tai.LogEvent, for: Tai.Events.OrderUpdated do
     event
     |> Map.take(keys)
     |> Map.put(:error_reason, event.error_reason |> inspect)
-    |> Map.put(:price, event.price |> Decimal.to_string(:normal))
-    |> Map.put(:qty, event.qty |> Decimal.to_string(:normal))
-    |> Map.put(:leaves_qty, event.leaves_qty |> Decimal.to_string(:normal))
-    |> Map.put(:cumulative_qty, event.cumulative_qty |> Decimal.to_string(:normal))
+    |> Map.put(:price, event.price && event.price |> Decimal.to_string(:normal))
+    |> Map.put(:qty, event.qty && event.qty |> Decimal.to_string(:normal))
+    |> Map.put(:leaves_qty, event.leaves_qty && event.leaves_qty |> Decimal.to_string(:normal))
+    |> Map.put(
+      :cumulative_qty,
+      event.cumulative_qty && event.cumulative_qty |> Decimal.to_string(:normal)
+    )
     |> Map.put(
       :enqueued_at,
       event.enqueued_at && event.enqueued_at |> DateTime.to_iso8601()
