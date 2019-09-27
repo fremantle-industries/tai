72d51782740c50ecb789acf0570eeb7e4ba45ba9
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-09-27 08:17:47 -0700

    OkEx process order book takes a product

--- apps/tai/test/tai/venue_adapters/okex/stream/process_order_book_test.exs
+++ apps/tai/test/tai/venue_adapters/okex/stream/process_order_book_test.exs
@@ -15,13 +15,8 @@ defmodule Tai.VenueAdapters.OkEx.Stream.ProcessOrderBookTest do
   setup do
     start_supervised!(Tai.PubSub)
     start_supervised!({Tai.Events, 1})
-
     {:ok, book_pid} = start_supervised({OrderBook, @product})
-
-    {:ok, store_pid} =
-      start_supervised(
-        {ProcessOrderBook, [venue_id: @venue, symbol: @symbol, venue_symbol: @venue_symbol]}
-      )
+    {:ok, store_pid} = start_supervised({ProcessOrderBook, @product})
 
     {:ok, %{book_pid: book_pid, store_pid: store_pid}}
   end
