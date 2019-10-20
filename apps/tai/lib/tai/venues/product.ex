266c97fec1f88da59d03a65afa8eb550227c11bb
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-10-20 13:33:02 -0700

    Add typedocs for new product attributes

--- apps/tai/lib/tai/venues/product.ex
+++ apps/tai/lib/tai/venues/product.ex
@@ -10,6 +10,32 @@ defmodule Tai.Venues.Product do
           | :settled
           | :unlisted
 
+  @typedoc """
+  The product to buy/sell or the underlying product used to buy/sell. For the product BTCUSD
+
+  - BTC = base asset
+  - USD = quote asset
+  """
+  @type asset :: String.t()
+
+  @typedoc """
+  The underlying value of the product. Spot products will always have a value = 1. Derivative products
+  can have values > 1.
+
+  e.g. OkEx quarterly futures product has a value of 100 where 1 contract represents $100 USD.
+  """
+  @type value :: Decimal.t()
+
+  @typedoc """
+  A derivative contract where PnL settlement is a different asset to the base or quote assets.
+  """
+  @type quanto :: true | false
+
+  @typedoc """
+  A derivative contract where the PnL settlement is in the base asset, e.g. XBTUSD settles PnL in XBT
+  """
+  @type inverse :: true | false
+
   @type symbol :: atom
   @type venue_symbol :: String.t()
   @type type :: :spot | :future | :swap | :option
@@ -18,8 +44,8 @@ defmodule Tai.Venues.Product do
           symbol: symbol,
           venue_symbol: venue_symbol,
           alias: String.t() | nil,
-          base: String.t(),
-          quote: String.t(),
+          base: asset,
+          quote: asset,
           status: status,
           type: type,
           price_increment: Decimal.t(),
@@ -29,9 +55,9 @@ defmodule Tai.Venues.Product do
           min_notional: Decimal.t() | nil,
           max_price: Decimal.t() | nil,
           max_size: Decimal.t() | nil,
-          value: Decimal.t(),
-          is_quanto: boolean,
-          is_inverse: boolean,
+          value: value,
+          is_quanto: quanto,
+          is_inverse: inverse,
           maker_fee: Decimal.t() | nil,
           taker_fee: Decimal.t() | nil
         }
