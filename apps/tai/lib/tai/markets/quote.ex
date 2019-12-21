1071a3f4edd285bed31e8ee55310c8659e2e9a0f
Alex Kwiatkowski <alex+git@rival-studios.com>
2019-12-20 22:25:39 -0800

    Add mid price to market quote

--- apps/tai/lib/tai/markets/quote.ex
+++ apps/tai/lib/tai/markets/quote.ex
@@ -24,8 +24,37 @@ defmodule Tai.Markets.Quote do
             last_received_at: nil,
             last_venue_timestamp: nil
 
+  @spec inside_bid(t) :: price_point | nil
   def inside_bid(market_quote), do: market_quote.bids |> List.first()
+
+  @spec inside_ask(t) :: price_point | nil
   def inside_ask(market_quote), do: market_quote.asks |> List.first()
+
+  @spec mid_price(t) :: {:ok, Decimal.t()} | {:error, :no_inside_bid | :no_inside_ask}
+  @spec mid_price(bid :: price_point, ask :: price_point) ::
+          {:ok, Decimal.t()} | {:error, :no_inside_bid | :no_inside_ask}
+  def mid_price(%Quote{} = market_quote) do
+    bid = market_quote |> inside_bid
+    ask = market_quote |> inside_ask
+    mid_price(bid, ask)
+  end
+
+  @two Decimal.new(2)
+  def mid_price(%PricePoint{} = bid, %PricePoint{} = ask) do
+    ask_price = Decimal.cast(ask.price)
+    bid_price = Decimal.cast(bid.price)
+
+    mid =
+      ask_price
+      |> Decimal.sub(bid_price)
+      |> Decimal.div(@two)
+      |> Decimal.add(bid_price)
+
+    {:ok, mid}
+  end
+
+  def mid_price(nil, %PricePoint{}), do: {:error, :no_inside_bid}
+  def mid_price(%PricePoint{}, nil), do: {:error, :no_inside_ask}
 end
 
 defimpl Stored.Item, for: Tai.Markets.Quote do
--- apps/tai/test/tai/markets/quote_test.exs
+++ apps/tai/test/tai/markets/quote_test.exs
@@ -15,4 +15,26 @@ defmodule Tai.Markets.QuoteTest do
 
     assert Quote.inside_ask(market_quote) == inside_ask
   end
+
+  test ".mid_price/1 is the price between the bid and ask" do
+    inside_bid = struct(PricePoint, price: 101, size: 2)
+    inside_ask = struct(PricePoint, price: 102, size: 10)
+    market_quote = struct(Quote, bids: [inside_bid], asks: [inside_ask])
+
+    assert Quote.mid_price(market_quote) == {:ok, Decimal.new("101.5")}
+  end
+
+  test ".mid_price/1 returns an error when there is no inside bid" do
+    inside_ask = struct(PricePoint, price: 102, size: 10)
+    market_quote = struct(Quote, bids: [], asks: [inside_ask])
+
+    assert Quote.mid_price(market_quote) == {:error, :no_inside_bid}
+  end
+
+  test ".mid_price/1 returns an error when there is no inside ask" do
+    inside_bid = struct(PricePoint, price: 101, size: 2)
+    market_quote = struct(Quote, bids: [inside_bid], asks: [])
+
+    assert Quote.mid_price(market_quote) == {:error, :no_inside_ask}
+  end
 end
