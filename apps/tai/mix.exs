ba44265c06615fac4178c1c648aab448dbfd0322
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-01-03 17:10:42 -0800

    Release 0.0.47

--- apps/tai/mix.exs
+++ apps/tai/mix.exs
@@ -4,7 +4,7 @@ defmodule Tai.Mixfile do
   def project do
     [
       app: :tai,
-      version: "0.0.46",
+      version: "0.0.47",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TaiMonorepo.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.0.46",
+      version: "0.0.47",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
