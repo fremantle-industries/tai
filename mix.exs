0dbeed6e5d957d4084d69921e492561aa19edb5b
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-01-05 23:26:36 -0800

    Release 0.0.48

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TaiMonorepo.MixProject do
   def project do
     [
       apps_path: "apps",
-      version: "0.0.47",
+      version: "0.0.48",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: aliases(),
