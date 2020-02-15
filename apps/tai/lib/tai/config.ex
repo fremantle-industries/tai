58db90c15b8c5c40524e9adc35db6acf5952ea38
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-02-15 10:48:53 -0800

    DRY up config parsing

--- apps/tai/lib/tai/config.ex
+++ apps/tai/lib/tai/config.ex
@@ -35,28 +35,20 @@ defmodule Tai.Config do
   )a
 
   def parse(env \\ Application.get_all_env(:tai)) do
-    adapter_timeout = Keyword.get(env, :adapter_timeout, 10_000)
-    advisor_groups = Keyword.get(env, :advisor_groups, %{})
-    broadcast_change_set = !!Keyword.get(env, :broadcast_change_set)
-    venue_boot_handler = Keyword.get(env, :venue_boot_handler, Tai.Venues.BootHandler)
-    send_orders = !!Keyword.get(env, :send_orders)
-    venues = Keyword.get(env, :venues, %{})
-
-    event_registry_partitions =
-      Keyword.get(env, :event_registry_partitions, System.schedulers_online())
-
-    system_bus_registry_partitions =
-      Keyword.get(env, :system_bus_registry_partitions, System.schedulers_online())
+    schedulers_online = System.schedulers_online()
 
     %Tai.Config{
-      adapter_timeout: adapter_timeout,
-      advisor_groups: advisor_groups,
-      broadcast_change_set: broadcast_change_set,
-      event_registry_partitions: event_registry_partitions,
-      system_bus_registry_partitions: system_bus_registry_partitions,
-      venue_boot_handler: venue_boot_handler,
-      send_orders: send_orders,
-      venues: venues
+      adapter_timeout: get(env, :adapter_timeout, 10_000),
+      advisor_groups: get(env, :advisor_groups, %{}),
+      broadcast_change_set: !!get(env, :broadcast_change_set),
+      event_registry_partitions: get(env, :event_registry_partitions, schedulers_online),
+      send_orders: !!get(env, :send_orders),
+      system_bus_registry_partitions:
+        get(env, :system_bus_registry_partitions, schedulers_online),
+      venue_boot_handler: get(env, :venue_boot_handler, Tai.Venues.BootHandler),
+      venues: get(env, :venues, %{})
     }
   end
+
+  defp get(env, key, default \\ nil), do: Keyword.get(env, key, default)
 end
