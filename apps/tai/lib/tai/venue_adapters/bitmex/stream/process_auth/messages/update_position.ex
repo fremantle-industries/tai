8ed314b196958fdc76009b1625273e5cb08b1338
Alex Kwiatkowski <alex+git@rival-studios.com>
2020-03-13 16:32:00 -0700

    WIP: BitMEX update position

--- apps/tai/lib/tai/venue_adapters/bitmex/stream/process_auth/messages/update_position.ex
+++ apps/tai/lib/tai/venue_adapters/bitmex/stream/process_auth/messages/update_position.ex
@@ -8,7 +8,26 @@ defmodule Tai.VenueAdapters.Bitmex.Stream.ProcessAuth.Messages.UpdatePosition do
   defstruct ~w(data)a
 
   defimpl ProcessAuth.Message do
+    require Logger
+
+    # %{
+    #   "account" => 158_677,
+    #   "currency" => "XBt",
+    #   "currentQty" => 1,
+    #   "currentTimestamp" => "2020-03-13T23:30:05.364Z",
+    #   "lastPrice" => 5656.81,
+    #   "liquidationPrice" => 1,
+    #   "maintMargin" => 138,
+    #   "markPrice" => 5656.81,
+    #   "posComm" => 14,
+    #   "posMaint" => 123,
+    #   "posMargin" => 5473,
+    #   "symbol" => "XBTUSD",
+    #   "timestamp" => "2020-03-13T23:30:05.364Z"
+    # }
+
     def process(message, _received_at, _state) do
+      Logger.info("================= UPDATE POSITION message data: #{inspect(message.data)}")
       :ok
     end
   end
