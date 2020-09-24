dd3d5d9541e247719cfa2148f5d0289a5090b035
Yuri Kovalov <yuri@yurikoval.com>
2020-09-24 14:02:02 +0800

    Extract event logging into its own module with interface

--- apps/tai/lib/tai/events_logger.ex
+++ apps/tai/lib/tai/events_logger.ex
@@ -1,15 +1,37 @@
 defmodule Tai.EventsLogger do
   use GenServer
-  require Logger
 
   defmodule State do
     @type t :: %State{
             id: atom,
-            events: module
+            events: module,
+            logger: module
           }
 
-    @enforce_keys ~w(id events)a
-    defstruct ~w(id events)a
+    @enforce_keys ~w(id events logger)a
+    defstruct ~w(id events logger)a
+  end
+
+  defmodule DefaultLogger do
+    @behaviour Tai.EventsLogger
+
+    require Logger
+
+    def log(:error, event) do
+      event |> TaiEvents.Event.encode!() |> Logger.error()
+    end
+
+    def log(:warn, event) do
+      event |> TaiEvents.Event.encode!() |> Logger.warn()
+    end
+
+    def log(:info, event) do
+      event |> TaiEvents.Event.encode!() |> Logger.info()
+    end
+
+    def log(:debug, event) do
+      event |> TaiEvents.Event.encode!() |> Logger.debug()
+    end
   end
 
   @type event :: map
@@ -17,15 +39,19 @@ defmodule Tai.EventsLogger do
 
   @default_id :default
   @default_events TaiEvents
+  @default_logger DefaultLogger
 
   def start_link(args) do
     id = Keyword.get(args, :id, @default_id)
     name = :"#{__MODULE__}_#{id}"
     events = Keyword.get(args, :events, @default_events)
-    state = %State{id: id, events: events}
+    logger = Keyword.get(args, :logger) || @default_logger
+    state = %State{id: id, events: events, logger: logger}
 
     GenServer.start_link(__MODULE__, state, name: name)
   end
+
+  @callback log(level, event) :: :ok
 
   @spec init(State.t()) :: {:ok, State.t()}
   def init(state) do
@@ -34,23 +60,8 @@ defmodule Tai.EventsLogger do
   end
 
   @spec handle_info({TaiEvents.Event, event, level}, State.t()) :: {:noreply, State.t()}
-  def handle_info({TaiEvents.Event, event, :error}, state) do
-    event |> TaiEvents.Event.encode!() |> Logger.error()
-    {:noreply, state}
-  end
-
-  def handle_info({TaiEvents.Event, event, :warn}, state) do
-    event |> TaiEvents.Event.encode!() |> Logger.warn()
-    {:noreply, state}
-  end
-
-  def handle_info({TaiEvents.Event, event, :info}, state) do
-    event |> TaiEvents.Event.encode!() |> Logger.info()
-    {:noreply, state}
-  end
-
-  def handle_info({TaiEvents.Event, event, :debug}, state) do
-    event |> TaiEvents.Event.encode!() |> Logger.debug()
+  def handle_info({TaiEvents.Event, event, level}, state) do
+    state.logger.log(level, event)
     {:noreply, state}
   end
 end
