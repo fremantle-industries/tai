436578842eda37ff25f3e90dd5a1951216951382
Alex <alex+git@fremantle.io>
2021-03-16 13:50:12 -0700

    Release v0.0.59

--- apps/examples/mix.exs
+++ apps/examples/mix.exs
@@ -4,7 +4,7 @@ defmodule Examples.MixProject do
   def project do
     [
       app: :examples,
-      version: "0.0.58",
+      version: "0.0.59",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
