1ee35c41f08ef152a227473f6faf44279ebce57b
Alex <alex+git@fremantle.io>
2021-03-17 10:00:11 -0700

    Use [] for struct definition

--- apps/tai/lib/tai/events/stream_subscribe_ok.ex
+++ apps/tai/lib/tai/events/stream_subscribe_ok.ex
@@ -10,6 +10,6 @@ defmodule Tai.Events.StreamSubscribeOk do
           received_at: DateTime.t()
         }
 
-  @enforce_keys ~w(venue channel_name venue_symbols received_at)a
-  defstruct ~w(venue channel_name venue_symbols received_at)a
+  @enforce_keys ~w[venue channel_name venue_symbols received_at]a
+  defstruct ~w[venue channel_name venue_symbols received_at]a
 end
