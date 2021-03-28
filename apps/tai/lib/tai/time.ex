229486cf400b34946316d42d58673fafc7f94801
Yuri Kovalov <yuri@yurikoval.com>
2021-03-28 14:37:51 +0800

    Convert monotonic to datetime without tuple

--- apps/tai/lib/tai/time.ex
+++ apps/tai/lib/tai/time.ex
@@ -7,9 +7,17 @@ defmodule Tai.Time do
   @spec monotonic_time :: integer
   def monotonic_time, do: System.monotonic_time(@monotonic_unit)
 
-  @spec monotonic_to_date_time(integer, :native | System.time_unit(), Calendar.calendar()) :: {:ok, DateTime.t()} | {:error, atom}
+  @spec monotonic_to_date_time(integer, :native | System.time_unit(), Calendar.calendar()) ::
+          {:ok, DateTime.t()} | {:error, atom}
   def monotonic_to_date_time(mono, unit \\ @monotonic_unit, calendar \\ Calendar.ISO) do
     (mono + System.time_offset(unit))
     |> DateTime.from_unix(unit, calendar)
+  end
+
+  @spec monotonic_to_date_time!(integer, :native | System.time_unit(), Calendar.calendar()) ::
+          DateTime.t()
+  def monotonic_to_date_time!(mono, unit \\ @monotonic_unit, calendar \\ Calendar.ISO) do
+    {:ok, value} = monotonic_to_date_time(mono, unit, calendar)
+    value
   end
 end
