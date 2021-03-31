8b1c5956477ce5ed762d3b2a71b63d66e5b9f032
Alex <alex+git@fremantle.io>
2021-03-30 22:06:28 -0700

    Add advisor terminate callback

--- apps/tai/lib/tai/advisors/supervisor.ex
+++ apps/tai/lib/tai/advisors/supervisor.ex
@@ -5,15 +5,16 @@ defmodule Tai.Advisors.Supervisor do
     DynamicSupervisor.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
-  def init(_) do
-    DynamicSupervisor.init(strategy: :one_for_one)
-  end
-
   def start_advisor(spec) do
     DynamicSupervisor.start_child(__MODULE__, spec)
   end
 
   def terminate_advisor(pid) when is_pid(pid) do
     DynamicSupervisor.terminate_child(__MODULE__, pid)
+  end
+
+  @impl true
+  def init(_) do
+    DynamicSupervisor.init(strategy: :one_for_one)
   end
 end
