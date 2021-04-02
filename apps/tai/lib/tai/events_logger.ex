e63c40e5f1b7e301ced0f4652b119f576f18fdc8
Alex <alex+git@fremantle.io>
2021-04-02 15:47:50 -0700

    Fix warning from definition order of Tai.EventsLogger.DefaultLogger

--- apps/tai/lib/tai/events_logger.ex
+++ apps/tai/lib/tai/events_logger.ex
@@ -1,6 +1,11 @@
 defmodule Tai.EventsLogger do
   use GenServer
 
+  @type event :: struct
+  @type level :: TaiEvents.level()
+
+  @callback log(level, event) :: term
+
   defmodule State do
     @type t :: %State{
             id: atom,
@@ -8,38 +13,13 @@ defmodule Tai.EventsLogger do
             logger: module
           }
 
-    @enforce_keys ~w(id events logger)a
-    defstruct ~w(id events logger)a
+    @enforce_keys ~w[id events logger]a
+    defstruct ~w[id events logger]a
   end
-
-  defmodule DefaultLogger do
-    @behaviour Tai.EventsLogger
-
-    require Logger
-
-    def log(:error, event) do
-      event |> TaiEvents.Event.encode!() |> Logger.error()
-    end
-
-    def log(:warn, event) do
-      event |> TaiEvents.Event.encode!() |> Logger.warn()
-    end
-
-    def log(:info, event) do
-      event |> TaiEvents.Event.encode!() |> Logger.info()
-    end
-
-    def log(:debug, event) do
-      event |> TaiEvents.Event.encode!() |> Logger.debug()
-    end
-  end
-
-  @type event :: map
-  @type level :: TaiEvents.level()
 
   @default_id :default
   @default_events TaiEvents
-  @default_logger DefaultLogger
+  @default_logger Tai.EventsLogger.DefaultLogger
 
   def start_link(args) do
     id = Keyword.get(args, :id, @default_id)
@@ -51,8 +31,6 @@ defmodule Tai.EventsLogger do
     GenServer.start_link(__MODULE__, state, name: name)
   end
 
-  @callback log(level, event) :: :ok
-
   @spec init(State.t()) :: {:ok, State.t()}
   def init(state) do
     state.events.firehose_subscribe()
@@ -63,5 +41,27 @@ defmodule Tai.EventsLogger do
   def handle_info({TaiEvents.Event, event, level}, state) do
     state.logger.log(level, event)
     {:noreply, state}
+  end
+end
+
+defmodule Tai.EventsLogger.DefaultLogger do
+  @behaviour Tai.EventsLogger
+
+  require Logger
+
+  def log(:error, event) do
+    event |> TaiEvents.Event.encode!() |> Logger.error()
+  end
+
+  def log(:warn, event) do
+    event |> TaiEvents.Event.encode!() |> Logger.warn()
+  end
+
+  def log(:info, event) do
+    event |> TaiEvents.Event.encode!() |> Logger.info()
+  end
+
+  def log(:debug, event) do
+    event |> TaiEvents.Event.encode!() |> Logger.debug()
   end
 end
