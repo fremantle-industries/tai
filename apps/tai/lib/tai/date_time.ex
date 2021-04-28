f22f1586680a7b0b9db5ad8bc1c72b2163106449
Alex <alex+git@fremantle.io>
2021-04-28 10:40:32 -0700

    Add Tai.DateTime.timestamp/1 proxy wrapper

--- apps/tai/lib/tai/date_time.ex
+++ apps/tai/lib/tai/date_time.ex
@@ -1,10 +1,15 @@
 defmodule Tai.DateTime do
-  @spec min(DateTime.t, DateTime.t) :: DateTime.t
+  @spec timestamp(Calendar.calendar()) :: DateTime.t()
+  def timestamp(calendar \\ Calendar.ISO) do
+    DateTime.utc_now(calendar)
+  end
+
+  @spec min(DateTime.t(), DateTime.t()) :: DateTime.t()
   def min(a, b) do
     [a, b] |> Enum.sort({:asc, DateTime}) |> List.first()
   end
 
-  @spec max(DateTime.t, DateTime.t) :: DateTime.t
+  @spec max(DateTime.t(), DateTime.t()) :: DateTime.t()
   def max(a, b) do
     [a, b] |> Enum.sort({:desc, DateTime}) |> List.first()
   end
--- apps/tai/test/tai/date_time_test.exs
+++ apps/tai/test/tai/date_time_test.exs
@@ -1,8 +1,15 @@
 defmodule Tai.DateTimeTest do
   use ExUnit.Case, async: false
+  import Mock
 
   @today DateTime.utc_now()
   @yesterday DateTime.utc_now() |> Timex.shift(days: -1)
+
+  test ".timestamp/1 proxies to DateTime.utc_now/1" do
+    with_mock DateTime, utc_now: fn _ -> ~U[2020-01-02 03:04:05.123456Z] end do
+      assert Tai.DateTime.timestamp(Calendar.ISO) == ~U[2020-01-02 03:04:05.123456Z]
+    end
+  end
 
   test ".min/1 returns the earliest value" do
     assert Tai.DateTime.min(@today, @yesterday) == @yesterday
