1d0f9d64325ab9fb4c220dfb67011cd8a9e2f1e3
Alex <alex+git@fremantle.io>
2021-05-19 12:53:45 -0700

    Bump ex_binance to v0.0.8 for Erlang 24 support

--- apps/tai/lib/tai/venue_adapters/binance/create_order.ex
+++ apps/tai/lib/tai/venue_adapters/binance/create_order.ex
@@ -13,15 +13,17 @@ defmodule Tai.VenueAdapters.Binance.CreateOrder do
     venue_side = side |> Atom.to_string() |> String.upcase()
     credentials = struct!(ExBinance.Credentials, credentials)
 
-    order.venue_product_symbol
-    |> ExBinance.Private.create_order(
-      venue_side,
-      @limit,
-      order.qty,
-      order.price,
-      venue_time_in_force,
-      credentials
-    )
+    %ExBinance.Rest.Requests.CreateOrderRequest{
+      new_client_order_id: order.client_id,
+      symbol: order.venue_product_symbol,
+      side: venue_side,
+      type:    @limit,
+      quantity:     order.qty,
+      quote_order_qty: order.qty,
+      price: order.price,
+      time_in_force: venue_time_in_force
+    }
+    |> ExBinance.Private.create_order(credentials)
     |> parse_response(order)
   end
 
@@ -29,17 +31,13 @@ defmodule Tai.VenueAdapters.Binance.CreateOrder do
   defp to_venue_time_in_force(:fok), do: "FOK"
   defp to_venue_time_in_force(:ioc), do: "IOC"
 
-  defp leaves_qty(:filled, _, _), do: Decimal.new(0)
-  defp leaves_qty(:expired, _, _), do: Decimal.new(0)
-  defp leaves_qty(:open, orig_qty, cum_qty), do: orig_qty |> Decimal.sub(cum_qty)
-
-  defp parse_response({:ok, %ExBinance.Responses.CreateOrder{} = binance_response}, _) do
+  defp parse_response({:ok, %ExBinance.Rest.Responses.CreateOrderResponse{} = binance_response}, _) do
     received_at = Tai.Time.monotonic_time()
     venue_order_id = binance_response.order_id |> Integer.to_string()
     status = binance_response.status |> OrderStatus.from_venue()
     original_size = binance_response.orig_qty |> Decimal.new() |> Decimal.normalize()
     cumulative_qty = binance_response.executed_qty |> Decimal.new() |> Decimal.normalize()
-    leaves_qty = leaves_qty(status, original_size, cumulative_qty)
+    leaves_qty = calc_leaves_qty(status, original_size, cumulative_qty)
     venue_timestamp = binance_response.transact_time |> DateTime.from_unix!(:millisecond)
 
     response = %Orders.Responses.Create{
@@ -62,4 +60,8 @@ defmodule Tai.VenueAdapters.Binance.CreateOrder do
     do: {:error, :insufficient_balance}
 
   defp parse_response({:error, reason}, _), do: {:error, {:unhandled, reason}}
+
+  defp calc_leaves_qty(:filled, _, _), do: Decimal.new(0)
+  defp calc_leaves_qty(:expired, _, _), do: Decimal.new(0)
+  defp calc_leaves_qty(:open, orig_qty, cum_qty), do: orig_qty |> Decimal.sub(cum_qty)
 end
