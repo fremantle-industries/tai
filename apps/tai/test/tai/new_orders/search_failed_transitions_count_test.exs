61fcc01349b564bc609c1ae715305df60d32acf2
Alex <alex+git@fremantle.io>
2021-06-17 22:23:18 -0700

    Test failed order transition search

---
+++ apps/tai/test/tai/new_orders/search_failed_transitions_count_test.exs
@@ -0,0 +1,12 @@
+defmodule Tai.NewOrders.SearchFailedTransitionsCountTest do
+  use Tai.TestSupport.DataCase, async: false
+
+  test "returns the count of failed order transitions matching the search query" do
+    {:ok, order} = create_order()
+    {:ok, _} = create_failed_order_transition(order.client_id, :error, "cancel")
+    {:ok, _} = create_failed_order_transition(order.client_id, :error, "cancel")
+    {:ok, _} = create_failed_order_transition(order.client_id, :error, "cancel")
+
+    assert Tai.NewOrders.search_failed_transitions_count(order.client_id, nil) == 3
+  end
+end
