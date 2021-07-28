33d23b468b3d8f8ef1b65d8e7b2200732b3a9891
Alex <alex+git@fremantle.io>
2021-07-28 09:22:04 -0700

    Match amend_price_and_qty with venue_order_id directly

--- apps/tai/lib/tai/test_support/mocks/responses/orders/good_till_cancel.ex
+++ apps/tai/lib/tai/test_support/mocks/responses/orders/good_till_cancel.ex
@@ -129,9 +129,14 @@ defmodule Tai.TestSupport.Mocks.Responses.Orders.GoodTillCancel do
     |> Mocks.Server.insert(order_response)
   end
 
-  @deprecated "Use amend_price_and_qty with venue_order_id instead."
-  def amend_price_and_qty(order, price, qty) do
-    amend_price_and_qty(order.venue_order_id, price, qty)
+  @deprecated "Use amend_accepted/3 with venue_order_id instead."
+  def amend_price_and_qty(%Order{} = order, price, qty) do
+    amend_accepted(order.venue_order_id, %{price: price, qty: qty})
+  end
+
+  @deprecated "Use amend_accepted/3."
+  def amend_price_and_qty(venue_order_id, price, qty) do
+    amend_accepted(venue_order_id, %{price: price, qty: qty})
   end
 
   @spec amend_bulk_accepted([{order, %{}}], [map]) :: :ok
