6dd939d81c7f47df2bc34e7a56530ced2b164785
alex <alex+git@fremantle.io>
2021-11-07 18:04:24 -0800

    Release v0.0.71

--- apps/examples/mix.exs
+++ apps/examples/mix.exs
@@ -4,7 +4,7 @@ defmodule Examples.MixProject do
   def project do
     [
       app: :examples,
-      version: "0.0.70",
+      version: "0.0.71",
       elixir: "~> 1.11",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
