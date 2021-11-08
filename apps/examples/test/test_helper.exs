5699551e199f77d17d01fa9eb6ad053e5e609eef
alex <alex+git@fremantle.io>
2021-11-08 10:01:35 -0800

    Disable ExUnitNotifier on CI

--- apps/examples/test/test_helper.exs
+++ apps/examples/test/test_helper.exs
@@ -1,2 +1,5 @@
-ExUnit.configure(formatters: [ExUnit.CLIFormatter, ExUnitNotifier])
+base_formatters = [ExUnit.CLIFormatter]
+formatters = if System.get_env("CI") == "true", do: base_formatters, else: base_formatters ++ [ExUnitNotifier]
+ExUnit.configure(formatters: formatters)
+Ecto.Adapters.SQL.Sandbox.mode(Tai.Orders.OrderRepo, :manual)
 ExUnit.start()
